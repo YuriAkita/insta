53946d08228493027f8ce4205fe059a29e964231
akitayuri <nmnl_y.bgux@icloud.com>
2021-06-05 21:21:42 +0900

    セッションログイン画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   resources :blogs
   resources :users, only: [:new, :create, :show, :edit, :update]
+  resources :sessions, only: [:new, :create, :destroy]
 end
