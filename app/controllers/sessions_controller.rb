d8115d00f29942a53955f5c98117aa1a30c75d27
akitayuri <nmnl_y.bgux@icloud.com>
2021-06-05 21:40:52 +0900

    add header

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  skip_before_action :login_required, only: [:new, :create]
+  
   def new
   end
 
@@ -18,5 +20,5 @@ class SessionsController < ApplicationController
     flash[:notice] = 'ログアウトしました'
     redirect_to new_session_path
   end
-  
+
 end
