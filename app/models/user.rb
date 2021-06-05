831c8d523ca765244ef4f536bd7ec1b862c115f1
akitayuri <nmnl_y.bgux@icloud.com>
2021-06-06 01:45:37 +0900

    add フォーム ボタンのCSS 重複アドレスのバリテーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ApplicationRecord
   mount_uploader :profile_image, ImageUploader
   validates :profile_image, presence: true
   validates :name,  presence: true, length: { maximum: 30 }
-  validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :email, presence: true, length: { maximum: 255 },uniqueness: true,format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase! }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
