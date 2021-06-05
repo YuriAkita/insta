6df55631739f7f9f7405ba19454afe927fb3319b
akitayuri <nmnl_y.bgux@icloud.com>
2021-06-05 20:24:56 +0900

    ブログ機能の見た目変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   mount_uploader :profile_image, ImageUploader
+  validates :name,  presence: true, length: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 255 }
 end
