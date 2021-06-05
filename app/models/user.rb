54244be6250d19d38b596684e110efac27a39b94
akitayuri <nmnl_y.bgux@icloud.com>
2021-06-05 22:49:07 +0900

    add お気に入り機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   mount_uploader :profile_image, ImageUploader
+  validates :profile_image, presence: true
   validates :name,  presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
@@ -7,4 +8,5 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
   has_many :blogs
+  has_many :favorites, dependent: :destroy
 end
