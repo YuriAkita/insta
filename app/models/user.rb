b9cba0d4cec2927a3a7f7bf57956bcfd1515eba5
akitayuri <nmnl_y.bgux@icloud.com>
2021-06-05 20:29:56 +0900

    ログインシステム　ユニーク制約作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   mount_uploader :profile_image, ImageUploader
   validates :name,  presence: true, length: { maximum: 30 }
-  validates :email, presence: true, length: { maximum: 255 }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 end
