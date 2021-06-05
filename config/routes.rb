e1c63003228ea9c3240befb4adfb08d8867cef33
akitayuri <nmnl_y.bgux@icloud.com>
2021-06-05 22:29:53 +0900

    add 投稿確認画面への遷移

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get 'sessions/new'
-  resources :blogs
+  resources :blogs do
+    collection do
+      post :confirm
+    end
+  end
   resources :users, only: [:new, :create, :show, :edit, :update]
   resources :sessions, only: [:new, :create, :destroy]
 end
